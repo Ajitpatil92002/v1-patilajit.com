c67368aaff80fab24372dba25b005d5f18b424c3
Ajit_Patil <patilajit020@gmail.com>
2022-07-31 14:32:45 +0530

    added more about

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ document.onreadystatechange = function () {
     document.querySelector("body").style.visibility = "hidden";
     document.querySelector("#loader").style.visibility = "visible";
   } else {
-    document.querySelector("#loader").style.display = "none";
-    document.querySelector("body").style.visibility = "visible";
+    setTimeout(() => {
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector("body").style.visibility = "visible";
+    }, 2000);
   }
 };
 
